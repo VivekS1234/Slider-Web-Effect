206dfa32f010e1e8f2a98a13d4689e924d705438
vivek kumar <v25462432@gmail.com>
2024-08-01 11:37:01 +0530

    second commit

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ let images = document.querySelector('.child1 .img3');
 
 function imageSlider(){
     document.querySelector('.child2 .fa-heart').addEventListener('click', function(){
-        images.style.transform = "translateY(-400px)";
+        images.style.transform = "translateY(-550px)";
         images.style.transitionDuration = "0.6s";
     })
     document.querySelector('.child2 .fa-thumbs-down').addEventListener('click', function(){
-        images.style.transform = "translateY(400px)";
+        images.style.transform = "translateY(550px)";
         images.style.transitionDuration = "0.6s";
     })
 }
-imageSlider();
+imageSlider()
+
+// git remote add origin https://github.com/VivekS1234/Slider-Web-Effect.git
+// git branch -M main
+// git push -u origin main
