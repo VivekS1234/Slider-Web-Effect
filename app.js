d8b9e1d8cb8a8fc39fc4f6e7a960316d381c1133
vivek kumar <v25462432@gmail.com>
2024-08-03 19:41:38 +0530

    Slider-Web

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 let tl = gsap.timeline();
 
 let icon1 = document.querySelector('.fa-fire')
+let icon2 = document.querySelector('.fa-thumbs-down')
 // let image2 = document.querySelector('.image2')
 
 function imageSlider(){
     icon1.addEventListener('click', function(){
         gsap.to('.image2',{
             scale: 1.2,
+            y: "-200px",
             opacity: 0,
             duration: 1,
+        })
+        gsap.from('.image2',{
+            scale:1,
+            duration: 1,
+            delay: 1.3
+        })
+    })
+    icon2.addEventListener('click', function(){
+        gsap.to('.image2',{
+            scale: 1.1,
+            y: "250px",
+            opacity: 0,
+            duration: 1,
+        })
+        gsap.from('.image2',{
+            scale:1,
+            duration: 1,
+            delay: 1.3
         })
     })
 }
