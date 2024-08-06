8700067665f3421b0acc8412a99e8134a5ccb043
vivek kumar <v25462432@gmail.com>
2024-08-06 19:45:27 +0530

    Clicking Effect APP

--- app.js
+++ app.js
@@ -2,34 +2,30 @@ let tl = gsap.timeline();
 
 let icon1 = document.querySelector('.fa-fire')
 let icon2 = document.querySelector('.fa-thumbs-down')
-// let image2 = document.querySelector('.image2')
 
-function imageSlider(){
-    icon1.addEventListener('click', function(){
-        gsap.to('.image2',{
-            scale: 1.2,
-            y: "-200px",
-            opacity: 0,
-            duration: 1,
-        })
-        gsap.from('.image2',{
-            scale:1,
-            duration: 1,
-            delay: 1.3
-        })
-    })
-    icon2.addEventListener('click', function(){
-        gsap.to('.image2',{
-            scale: 1.1,
-            y: "250px",
-            opacity: 0,
-            duration: 1,
-        })
-        gsap.from('.image2',{
-            scale:1,
-            duration: 1,
-            delay: 1.3
-        })
-    })
+let clickCount = 0;
+
+function imageSlider() {
+  icon1.addEventListener('click', function() {
+    if (clickCount === 0) {
+      gsap.to('.image2', {
+        scale: 1.5,
+        y: "-850px",
+        // opacity: 0, 
+        duration: 1.2
+      });
+      gsap.to('.image2', {
+        scale: 1,
+        y: "0px",
+        duration: 1,
+        delay: 1.2
+      });
+
+      clickCount++; 
+
+      icon1.removeEventListener('click', imageSlider);
+    }
+  });
 }
+
 imageSlider();
